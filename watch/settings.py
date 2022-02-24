2b00ff4fc600b42186449669e1fa9afd190903f9
Abhijithc145 <abhijithabhipgmr145@gmail.com>
2022-02-24 21:59:12 +0530

    added debug

--- watch/settings.py
+++ watch/settings.py
@@ -126,9 +126,14 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT =BASE_DIR/'static'
-STATICFILE_DIRS = [
-                    os.path.join(BASE_DIR,'static')
-                    ]    
+# STATICFILE_DIRS = [
+#                     os.path.join(BASE_DIR,'static')
+#                     ]    
+
+if DEBUG:
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
